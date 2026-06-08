fcc3dc29e447c9811b6b6ceac0ac4db3b14d75ba
abdallah <workabdallah400@gmail.com>
2026-06-08 20:35:26 +0300

    creat notes app

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
 
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return Scaffold(
 
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: MyApp()
     );
   }
 }
